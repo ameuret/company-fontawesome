5368eb73a782d9397fdc0d019586a43098f1c4fb
Arnaud Meuret <arnaud@meuret.net>
2017-07-12 15:51:33 +0200

    At this point everything seemed perfect

---
+++ README.md
@@ -0,0 +1,43 @@
+# company-fontawesome.el
+
+
+A [company-mode](https://github.com/company-mode/company-mode/)
+backend to autocomplete font awesome CSS class names **with icon
+previz** !
+
+![Actual FA icons shown in selection list](visualSelection.gif "Visual Selection FTW!")
+
+## Setup
+
+Copy `company-fontawesome.el` somewhere in your `load-path` then
+execute the following:
+
+```elisp
+(require 'company-fontawesome)
+(add-to-list 'company-backends 'company-fontawesome)
+```
+
+## FAQ
+
+The wrong icon is displayed:
+
+![Wrong icon is shown in selection list](fontConflict.jpg "Font conflict")
+
+Disable the conflicting font (usually one of font-mfizz or octicons)
+if you can't live with it until I find a way to force `company-mode`
+to display the completion list using the Font Awesome face exclusively.
+
+fa-yo
+
+## Caveat Emptor
+
+I finished the first iteration of this plug-in 90 minutes after writing my first
+`(message "Hello, world!")` Emacs Lisp program. And it was 2am. Expect
+rough edgez. :grin:
+
+## TODO
+
+- [ ] Cache candidates !
+- [ ] Offer to insert the character instead of the class name
+- [ ] Add completions for the modifier class names (e.g. fa-spin,
+  fa-border, etc.)
---
+++ company-fontawesome.el
@@ -0,0 +1,58 @@
+
+
+(require 'cl-lib)
+(require 'all-the-icons)
+
+;; (defvar company-custom-keywords
+;;   '(("foobar" "") ("foobaz" "")  ("foobarbaz" "")))
+
+(defun company-fontawesome--make-candidate (candidate)
+  (let ((text (concat "fa-" (car candidate)))
+        (meta (cdr candidate))) ;was cadr
+    (propertize text 'meta meta)))
+
+(defun company-fontawesome--candidates (prefix)
+  (message "Seriously ?")
+  (let (res)
+    (dolist (item (all-the-icons-faicon-data))
+      (when (string-prefix-p prefix (concat "fa-" (car item)))
+        (push (company-fontawesome--make-candidate item) res)))
+    res))
+
+(defun company-fontawesome--meta (candidate)
+  (format "%s -> %s"
+          (substring-no-properties candidate)
+          (get-text-property 0 'meta candidate)
+          ))
+
+(defun company-fontawesome--annotation (candidate)
+  (format "%s" (get-text-property 0 'meta candidate)))
+
+(defun company-fontawesome (command &optional arg &rest ignored)
+  (interactive (list 'interactive))
+  (cl-case command
+    (interactive (company-begin-backend 'company-fontawesome))
+    (prefix (company-grab-symbol-cons "\\.\\|->" 2))
+    (candidates (company-fontawesome--candidates arg))
+    (annotation (company-fontawesome--annotation arg))
+    (meta (company-fontawesome--meta arg))))
+
+(defun company-fontawesome-init ()
+  "Add fontawesome to the company backends."
+  (interactive)
+  (add-to-list 'company-backends 'company-fontawesome))
+
+(provide 'company-fontawesome)
+
+
+;; fa-user
+
+;; (all-the-icons-insert-icons-for 'faicon 1)
+
+;; (message "%s" (cdr(car (nth 42 (all-the-icons-faicon-data)))))
+;; (cdr(nth 42 (all-the-icons-faicon-data)))
+;; (type-of (cdr(nth 0 company-custom-keywords)))
+;; (type-of (cdr(nth 42 (all-the-icons-faicon-data))))
+
+;; (dolist (i (all-the-icons-faicon-data))
+;;    (message "%s: %s" (car i) (cdr i)))
