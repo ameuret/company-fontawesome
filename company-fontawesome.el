7bd61ff242c7fc2dbf0b2428be2af4164203a875
Arnaud Meuret <arnaud@meuret.net>
2017-07-14 01:21:56 +0200

    More doc and Flycheck compliance

--- company-fontawesome.el
+++ company-fontawesome.el
@@ -22,14 +22,19 @@
 (defconst company-fontawesome-version "0.1.0")
 
 (defun company-fontawesome--make-candidate (candidate)
+  "Build a propertized string from the CANDIDATE list.
+
+CANDIDATE must be formed as: (name . glyph).
+The icon name is prefixed with fa- and the glyph is stored under the meta prop."
+  
   (let (
         (text (concat "fa-" (car candidate)))
-        (meta (cdr candidate))
-        )
-    (setq text (propertize text 'meta meta)))
-  )
+        (meta (cdr candidate)))
+    (setq text (propertize text 'meta meta))))
 
 (defun company-fontawesome--candidates (prefix)
+  "Build the list of candidates that start with PREFIX."
+  
   (let (res)
     (dolist (item (all-the-icons-faicon-data))
       (when (string-prefix-p prefix (concat "fa-" (car item)))
@@ -40,18 +45,33 @@
     res))
 
 (defun company-fontawesome--meta (candidate)
+  "Build the string to be displayed when the CANDIDATE is highlighted.
+
+  Displayed in the echo area when the default frontend is used."
+  
   (format "%s -> %s"
           (substring-no-properties candidate)
           (get-text-property 0 'meta candidate)
           ))
 
 (defun company-fontawesome--annotation (candidate)
+  "Build the string showing the CANDIDATE glyph alongside the icon id."
+
   (setq res (format "%s" (get-text-property 0 'meta candidate)))
+  ;; This attempt to control the display (hoping to solve font
+  ;; conflicts) has proved insufficient. The face properties are reset
+  ;; later by company.el
   (put-text-property 0 1 'font-lock-face 'font-lock-warning-face res)
   res)
 
 (defun company-fontawesome (command &optional arg &rest ignored)
+  "Start the backend and act according to COMMAND.
+
+ARG can have various meanings depending on COMMAND but is often the
+candidate."
+  
   (interactive (list 'interactive))
+  
   ;; (message "Company cmd:%s" command)
   (cl-case command
     (interactive (company-begin-backend 'company-fontawesome))
@@ -67,6 +87,7 @@
 
 (defun company-fontawesome-init ()
   "Add fontawesome to the company backends."
+  
   (interactive)
   (add-to-list 'company-backends 'company-fontawesome))
 
